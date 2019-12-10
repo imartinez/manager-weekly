b34fc531796e80d5b9cc6ecb99e9b9083f99b2a8
imartinez <ivanmartit@gmail.com>
2019-12-10 21:49:52 +0100

    Initial app (Android) and core (Kotlin multiplatform) modules with working tests

---
+++ core/build.gradle.kts
@@ -0,0 +1,74 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    //select iOS target platform depending on the Xcode environment variables
+    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
+        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
+            ::iosArm64
+        else
+            ::iosX64
+
+    iOSTarget("ios") {
+        binaries {
+            framework {
+                baseName = "SharedCode"
+            }
+        }
+    }
+
+    jvm("android")
+
+    jvm("android").compilations["test"].defaultSourceSet {
+        dependencies {
+            implementation(kotlin("test", KotlinCompilerVersion.VERSION))
+            implementation(kotlin("test-junit", KotlinCompilerVersion.VERSION))
+        }
+    }
+
+    sourceSets["commonMain"].dependencies {
+        implementation(kotlin("stdlib-common"))
+    }
+
+    sourceSets["commonTest"].dependencies {
+        implementation(kotlin("test-common", KotlinCompilerVersion.VERSION))
+        implementation(kotlin("test-annotations-common", KotlinCompilerVersion.VERSION))
+    }
+
+    sourceSets["androidMain"].dependencies {
+        implementation(kotlin("stdlib"))
+    }
+}
+
+val packForXcode by tasks.creating(Sync::class) {
+    val targetDir = File(buildDir, "xcode-frameworks")
+
+    /// selecting the right configuration for the iOS 
+    /// framework depending on the environment
+    /// variables set by Xcode build
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val framework = kotlin.targets
+        .getByName<KotlinNativeTarget>("ios")
+        .binaries.getFramework(mode)
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+
+    from({ framework.outputDirectory })
+    into(targetDir)
+
+    /// generate a helpful ./gradlew wrapper with embedded Java path
+    doLast {
+        val gradlew = File(targetDir, "gradlew")
+        gradlew.writeText("#!/bin/bash\n"
+                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+                + "cd '${rootProject.rootDir}'\n"
+                + "./gradlew \$@\n")
+        gradlew.setExecutable(true)
+    }
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
---
+++ core/src/commonMain/kotlin/common.kt
@@ -0,0 +1,8 @@
+package com.imartinez.core
+
+//expect fun platformName(): String
+
+fun createApplicationScreenMessage() : String {
+    //return "Kotlin Rocks on ${platformName()}"
+    return "Kotlin Rocks!"
+}
